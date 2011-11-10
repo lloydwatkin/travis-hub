016303fede7a08372cea2aecfc85ca2d4177a2b9
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-11-10 01:27:03 +0100

    round in 1.8.7 takes no args?

--- lib/travis/hub.rb
+++ lib/travis/hub.rb
@@ -86,7 +86,7 @@ module Travis
         timing = Benchmark.realtime do
           ActiveRecord::Base.cache { yield }
         end
-        log notice("Completed in #{timing.round(4)} seconds")
+        log notice("Completed in #{timing.round} seconds")
       end
 
       def decode(payload)
