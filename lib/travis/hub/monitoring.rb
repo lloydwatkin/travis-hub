bc58ee5bd6c8335dc7bd73d08f3615424ed7ae41
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-11-30 20:09:36 +0100

    add transaction tracing just for the methods which handle the messages

--- lib/travis/hub/monitoring.rb
+++ lib/travis/hub/monitoring.rb
@@ -1,25 +1,21 @@
 module Travis
   class Hub
     module Monitoring
-      module HubInstrumentation
-        def receive(message, payload)
-          params = {
-            :message => message,
-            :payload => payload
-          }
-          perform_action_with_newrelic_trace(:category => :controller, :name => 'receive', :params => params) do
-            super
-          end
-        end
-      end
-
       def self.start
         puts "Starting New Relic with env:#{ENV['ENV']}"
         require 'newrelic_rpm'
 
-        Travis::Hub.class_eval do
+        Travis::Hub::Handler::Job.class_eval do
           include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-          include Travis::Hub::Monitoring::HubInstrumentation
+
+          add_transaction_tracer(:handle_log_update)
+          add_transaction_tracer(:handle_log_update)
+        end
+
+        Travis::Hub::Handler::Worker.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+
+          add_transaction_tracer(:handle)
         end
 
         NewRelic::Agent.manual_start(:env => ENV['ENV'])
