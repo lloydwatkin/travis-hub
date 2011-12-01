b67a9da8bd9669c4df5412b602f2f6ffb17d7281
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-12-01 01:35:05 +0100

    we need to monitor the actuall methods which send the notifications, not the async code

--- lib/travis/hub/monitoring.rb
+++ lib/travis/hub/monitoring.rb
@@ -5,6 +5,9 @@ module Travis
         puts "Starting New Relic with env:#{ENV['ENV']}"
         require 'newrelic_rpm'
 
+        #
+        # Add controller instrumentation to the AMQP message handlers
+        #
         Travis::Hub::Handler::Job.class_eval do
           include NewRelic::Agent::Instrumentation::ControllerInstrumentation
           add_transaction_tracer(:handle_log_update)
@@ -16,15 +19,42 @@ module Travis
           add_transaction_tracer(:handle)
         end
 
+
+        #
+        # Add task instrumentation to the background jobs
+        #
         require 'travis/notifications'
-        ['Pusher', 'Email', 'Irc', 'Campfire', 'Webhook'].each do |service|
-          eval <<-RUBY
-            Travis::Notifications::#{service}.class_eval do
-              include NewRelic::Agent::Instrumentation::ControllerInstrumentation
-              add_transaction_tracer(:notify, :category => :task)
-            end
-          RUBY
+        Travis::Notifications::Pusher.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer(:push, :category => :task)
         end
+
+        Travis::Notifications::Email.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer(:send_emails, :category => :task)
+        end
+
+        Travis::Notifications::Irc.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer(:send_irc_notifications, :category => :task)
+        end
+
+        Travis::Notifications::Campfire.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer(:send_webhook_notifications, :category => :task)
+        end
+
+        Travis::Notifications::Webhook.class_eval do
+          include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+          add_transaction_tracer(:send_webhook_notifications, :category => :task)
+        end
+
+        # [:Pusher, :Email, :Irc, :Campfire, :Webhook].each do |service|
+        #   Travis::Notifications.const_get(service).class_eval do
+        #     include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+        #     add_transaction_tracer(:notify, :category => :task)
+        #   end
+        # end
 
 
         NewRelic::Agent.manual_start(:env => ENV['ENV'])
